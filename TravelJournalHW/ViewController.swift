f38a81d6e7c65adc786b403117af2d942717b3be
artemsnoegin <snoegin@gmail.com>
2025-11-02 16:56:39 +0300

    init commit

---
+++ TravelJournalHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelJournalHW
+//
+//  Created by Артём Сноегин on 02.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+

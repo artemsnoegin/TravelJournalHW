3eb1dc105a249b1bd27c3ed69e780d85e1c0485d
artemsnoegin <snoegin@gmail.com>
2025-11-07 15:09:31 +0300

    create trips table

---
+++ TravelJournalHW/ViewControllers/TripsTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TripsTableViewController.swift
+//  TravelJournalHW
+//
+//  Created by Артём Сноегин on 07.11.2025.
+//
+
+import UIKit
+
+class TripsTableViewController: UITableViewController {
+    
+    private var trips = [Trip(name: "trip", about: "as", image: UIImage(), days: [])]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        title = "Trips"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+    }
+    
+    @objc private func addTapped() {
+        
+        let createTripVC = CreateTripViewController()
+        createTripVC.completion = { [weak self] trip in
+            self?.trips.append(trip)
+            self?.tableView.reloadData()
+        }
+        
+        navigationController?.pushViewController(createTripVC, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        trips.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = trips[indexPath.row].name
+        cell.textLabel?.font = .preferredFont(forTextStyle: .title2)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let selectedTrip = trips[indexPath.row]
+        
+        let tripVC = TripTableViewController(trip: selectedTrip)
+        navigationController?.pushViewController(tripVC, animated: true)
+    }
+}
